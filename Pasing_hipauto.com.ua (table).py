987608de113202b06b5fe612951f3cf6a4d377b6
SergeyPolischuk <polischuksg@gmail.com>
2019-03-25 14:41:08 +0200

    тест загрузки

--- Pasing_hipauto.com.ua (table).py
+++ Pasing_hipauto.com.ua (table).py
@@ -41,6 +41,7 @@ def get_page_data(html):
     brands_list = []
     for brand in brands:
         brand = str(brand)
+        brand = brand.replace('Ë', 'E')
         num_first_symbol = brand.find('>') + 1
         num_last_symbol = brand.find('</a>')
         brands_list.append(brand[num_first_symbol:num_last_symbol])
@@ -52,20 +53,18 @@ def get_page_data(html):
         num_last_symbol = article.find('</b>')
         articles_list.append(article[num_first_symbol:num_last_symbol])
 
-    count_position = max(len(brands_list), len(articles_list))
-
     descriptions_list = []
     for description in descriptions:
         description = str(description.find('a').contents).strip('[').strip("'")
         num_last_symbol = description.find('<') - 4
         descriptions_list.append(description[:num_last_symbol].strip(' ').rstrip(' '))
 
-    remainders_list = []
-    for remainder in remainders:
-        remainder = str(remainder.contents)
-        num_first_symbol = remainder.find('</span>') + 10
-        num_last_symbol = remainder.find('</td>') - 1
-        remainders_list.append(remainder[num_first_symbol:num_last_symbol].strip('  ').strip(' '))
+    # remainders_list = []
+    # for remainder in remainders:
+    #     remainder = str(remainder.contents)
+    #     num_first_symbol = remainder.find('</span>') + 10
+    #     num_last_symbol = remainder.find('</td>') - 1
+    #     remainders_list.append(remainder[num_first_symbol:num_last_symbol].strip('  ').strip(' '))
 
     prices_list = []
     for price in prices:
@@ -85,60 +84,48 @@ def get_page_data(html):
             article = ''
 
         try:
-            description =descriptions_list[num]
+            description = descriptions_list[num]
         except:
             description = ''
 
-        try:
-            remainder = remainders_list[num]
-        except:
-            remainder = ''
+        # try:
+        #     remainder = remainders_list[num]
+        # except:
+        #     remainder = ''
 
         try:
             price = prices_list[num]
         except:
             price = ''
 
-        good_row = {'brand': brand,
+        product_row = {'brand': brand,
             'artcile': article,
             'description': description,
-            'remainder': remainder,
+            # 'remainder': remainder,
             'price': price}
-        print(good_row)
+        data.append(product_row)
+    return data
 
-
-
-
-    # data = {"Brands": brands_list, "Articles": articles_list, "Descriptions": descriptions_list, "Remainders": remainders_list, "Prices": prices_list}
-    #
-    # max_n = max([len(x) for x in data.values()])
-    # for field in data:
-    #     data[field] += [''] * (max_n - len(data[field]))
-    #     print(data)
-
-
-    # проверка len для списка, если len < 542 - вводим ''
-
-    # for i in range(len(brands_list)):
-
-
-
-# def write_csv(i, data):
-#     with open('coinmarketcap.csv', 'a') as f:
-#         writer = csv.writer(f)
-#         writer.writerow((data['name'],
-#                          data['price']))
-#         print(i, data['name'], 'parsed')
-
-
+def write_csv(data):
+    with open('hipauto.csv', 'a') as f:
+        writer = csv.writer(f)
+        for key in data:
+            writer.writerow((
+                key['brand'],
+                key['artcile'],
+                key['description'],
+                # key['remainder'],
+                key['price']))
 
 def main():
     url = 'http://www.hipavto.com.ua/search/number/?article=OC+196&brand=34'
-    get_page_data(get_html(url))
+    all_data = get_page_data(get_html(url))
+    write_csv(all_data)
 
-    # for index, url in enumerate(all_data):
+    # for i, url in enumerate(all_data):
     #     html = get_html(url)
-    #     data = get_page_data(html)      #закомментировать для получения бренд/артикул/описание/цена/количество
+    #     data = get_page_data(html)
+    #     write_csv(i, data) #закомментировать для получения бренд/артикул/описание/цена/количество
 
 if __name__ == '__main__':
     main()
